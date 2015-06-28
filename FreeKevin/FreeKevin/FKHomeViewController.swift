6b23336e5ba6e0bfa2e47e0992afc701931cfbfc
naomihimley <naomihimley@gmail.com>
2015-06-27 20:34:09 -0500

    Enum and switch instead of all these ifs

--- FreeKevin/FreeKevin/FKHomeViewController.swift
+++ FreeKevin/FreeKevin/FKHomeViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class FKHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var actionsTableView: UITableView!
+    enum Cells : Int {
+        case Installation
+        case Use
+        case Practice
+    }
+    
+    //MARK: - Views -
     override func viewDidLoad() {
         super.viewDidLoad()
         //Set home screen UI
@@ -25,35 +32,37 @@ class FKHomeViewController: UIViewController, UITableViewDataSource, UITableView
         return 3;
     }
     
+    //MARK: - Table View Methods -
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier: "FKReuseIdentifier")
         cell.backgroundColor = UIColor.clearColor()
         cell.textLabel?.tintColor = UIColor.greenColor()
-        if (indexPath.row == 0) {
-            cell.textLabel?.text = "Installation Instructions"
-        }
-        else if (indexPath.row == 1 ){
-            cell.textLabel?.text = "Keyboard Use Instructions"
-        }
-        else {
-            cell.textLabel?.text = "Practice"
+        if let row = Cells(rawValue: indexPath.row){
+            switch (row) {
+            case .Installation:
+                cell.textLabel?.text = "Installation Instructions"
+            case .Use:
+                cell.textLabel?.text = "Keyboard Use Instructions"
+            case .Practice:
+                cell.textLabel?.text = "Practice"
+            }
         }
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if (indexPath.row == 0) {
-            let vc = self.storyboard!.instantiateViewControllerWithIdentifier("FKInstructionsStoryboardID") as! FKInstructionsViewController
-            self.presentViewController(vc, animated: true, completion: nil)
-        }
-        else if (indexPath.row == 1) {
-            //Present keyboard instructions
-            let vc = self.storyboard!.instantiateViewControllerWithIdentifier("FKKeyboardInstructionsStoryboardID") as! FKKeyboardInstructionsViewController
-            self.presentViewController(vc, animated: true, completion: nil)
-        }
-        else {
-            let vc = self.storyboard!.instantiateViewControllerWithIdentifier("FKPracticeStoryboardID") as! FKPracticeViewController
-            self.presentViewController(vc, animated: true, completion: nil)
+        if let row = Cells(rawValue: indexPath.row) {
+            switch (row) {
+            case .Installation:
+                let vc = self.storyboard!.instantiateViewControllerWithIdentifier("FKInstructionsStoryboardID") as! FKInstructionsViewController
+                self.presentViewController(vc, animated: true, completion: nil)
+            case .Use:
+                let vc = self.storyboard!.instantiateViewControllerWithIdentifier("FKKeyboardInstructionsStoryboardID") as! FKKeyboardInstructionsViewController
+                self.presentViewController(vc, animated: true, completion: nil)
+            case .Practice:
+                let vc = self.storyboard!.instantiateViewControllerWithIdentifier("FKPracticeStoryboardID") as! FKPracticeViewController
+                self.presentViewController(vc, animated: true, completion: nil)
+            }
         }
     }
 }
